27448d3139b268c2cc93cfa172f39b1ef0308f5c
Bob Breznak <bob.breznak@gmail.com>
2011-10-01 14:45:26 -0400

    Pluginifying existing script

--- wip.js
+++ wip.js
@@ -1,62 +1,77 @@
-$(document).ready(function() {
+(function($) {
+  $.wip = function(options) {
 
-  var wip_status = [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0];
-
-  jQuery.expr[':'].regex = function(elem, index, match) {
-      var matchParams = match[3].split(','),
-          validLabels = /^(data|css):/,
-          attr = {
-              method: matchParams[0].match(validLabels) ? 
-                          matchParams[0].split(':')[0] : 'attr',
-              property: matchParams.shift().replace(validLabels,'')
-          },
-          regexFlags = 'ig',
-          regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
-      return regex.test(jQuery(elem)[attr.method](attr.property));
-  }
-
-  wipit = function(e, index) {
-    switch(wip_status[index]){
-      case 0:
-        e.addClass('highlight');
-        e.removeClass('hidden');
-        break;
-      case 1:
-        e.addClass('hidden');
-        break;
-      default:
-        e.removeClass('hidden');
-        e.removeClass('highlight');
+    // Thanks to James Padolsey for regex parsing
+    // original source: http://james.padolsey.com/javascript/regex-selector-for-jquery/
+    jQuery.expr[':'].regex = function(elem, index, match) {
+        var matchParams = match[3].split(','),
+            validLabels = /^(data|css):/,
+            attr = {
+                method: matchParams[0].match(validLabels) ? 
+                            matchParams[0].split(':')[0] : 'attr',
+                property: matchParams.shift().replace(validLabels,'')
+            },
+            regexFlags = 'ig',
+            regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+        return regex.test(jQuery(elem)[attr.method](attr.property));
     }
-    wip_status[index] = (wip_status[index] + 1) % 3;
-  }
 
-  $('html').keydown(function(e) {
-    if(e.ctrlKey) {
-      element = null;
-      index = 0;
-
-      console.log(e.which);
-
-      if(e.which > 47 && e.which < 58) {
-        index = e.which - 48;
-        element = $('.wip_' + index);
+    get_status = function(e){
+      if(!e.hasClass('hidden') && e.hasClass('highlight')){
+        return 'highlighted';
       }
 
-      if(e.which === 87){
-        element = $('.wip');
+      if(e.hasClass('hidden') && !e.hasClass('highlight')){
+        return 'hidden';
       }
 
-      if(e.which === 80){
-        element = $('.piw');
-      }
-
-      if(element) {
-        wipit(element, index);
-      }
+      return 'normal';
     }
-  });
 
-  $(':regex(class,^wip_[1-9])').addClass('wip');
-  $(':regex(class,^piw_[1-9])').addClass('piw');
-});
+    var status = 0;
+    update = function(e){
+      switch(get_status(e)){
+        case 'normal':
+          e.removeClass('hidden');
+          e.addClass('highlight');
+          break;
+        case 'highlighted':
+          e.addClass('hidden');
+          e.removeClass('highlight');
+          break;
+        default:
+          e.removeClass('hidden');
+          e.removeClass('highlight');
+      }
+
+      status = (status + 1) % 3;
+    }
+
+    $('html').keydown(function(e) {
+      if(e.ctrlKey) {
+        console.log(e.which);
+        if(e.which === 87){
+          update($('.wip'));
+        }
+
+        if(e.which === 80){
+          update($('.piw'));
+        }
+
+        if(!e.shiftKey && (e.which > 47 && e.which < 58)) {
+          index = e.which - 48;
+          update($('.wip_' + index));
+        }
+
+        if(e.shiftKey && (e.which > 47 && e.which < 58)) {
+          index = e.which - 48;
+          update($('.piw_' + index));
+        }
+      }
+    });
+
+    $(':regex(class,^wip_[1-9])').addClass('wip');
+    $(':regex(class,^piw_[1-9])').addClass('piw');
+
+  }
+})( jQuery );
